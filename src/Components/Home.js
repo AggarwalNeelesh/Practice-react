aaef428440e0f24321fff6d2cc4f345cf6f6bef2
Neelesh Aggarwal <86107529+AggarwalNeelesh@users.noreply.github.com>
2023-04-09 17:09:23 +0530

    state with objects

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import {React, useState} from 'react'
+import { Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 
 function Home() {
+  const [data, setdata] = useState({name:"Neelesh", age:22, hobby:"anime"})
   return (
     <>
     <h1>Home</h1>
+    <input type="text" value={data.name} onChange={(e)=>setdata({...data, name:e.target.value})} />
+    <h1>{data.name}</h1>
+    <h1>{data.age}</h1>
+    <h1>{data.hobby}</h1>
     <Link to="/jokes">Go read some Jokes</Link>
     </>
   )
--- src/Components/Jokes.js
+++ src/Components/Jokes.js
@@ -3,15 +3,26 @@ import { Table } from "react-bootstrap";
 
 export default function Jokes(){
     const [data, setdata] = useState([]);
+    const [perdata, setperdata] = useState();
     useEffect(() => {
       fetch('https://v2.jokeapi.dev/joke/Any?amount=10').then((res)=>{
             res.json().then((rep)=>{
                 setdata(rep.jokes);
             })
       })
-    
-      
     }, [])
+    useEffect(() => {
+        fetch('http://localhost:8080/beatAll',{mode: 'no-cors',
+        headers: {
+             "Content-Type": "application/json"
+        }}).then((res)=>{
+            res.json().then((rep)=>{
+                // setperdata(rep); 
+                console.log("fucl");
+            })
+          })
+    }, [])
+    
     // url = https://www.eporner.com/api/v2/video/search/?query=teen&per_page=10&page=2&thumbsize=big&order=top-weekly&gay=1&lq=1&format=json
     // console.log(data[0]);
     // data.map((item, i)=>{
